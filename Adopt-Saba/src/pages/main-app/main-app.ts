064513733354e7c24057cfeb837942b240e49aa3
diaaldin <dia81994@gmail.com>
2018-04-16 15:59:50 +0300

    add

--- Adopt-Saba/src/pages/main-app/main-app.ts
+++ Adopt-Saba/src/pages/main-app/main-app.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
-import {ChangeDetectorRef} from '@angular/core'
+import {ChangeDetectorRef} from '@angular/core';
 import { Camera} from '@ionic-native/camera';
-
-
 import { AlertController } from 'ionic-angular';
 import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview';
 
@@ -39,12 +37,17 @@ export class MainAppPage {
     toBack: true,
     alpha: 1
   };
-  hour=0;
-  minute=0;
-  second=0;
+
+  hourL=0;
+  minuteL=0;
+  secondL=0;
+  hourR=0;
+  minuteR=0;
+  secondR=0;
+  temp=true;
+  temp1=false;
   txt="Start";
   intrevalId:number;
- // intrevalDate:number; 
   intrevalDate= setInterval(() => {
    this.myDate = new Date(new Date().getTime()+(3*60*60*1000)).toISOString();
   },1000)
@@ -55,52 +58,132 @@ export class MainAppPage {
   }
   
   takePic(){
-   this.camera.getPicture({
-      destinationType: this.camera.DestinationType.DATA_URL,
-      targetWidth: 1000,
-      targetHeight: 1000
-  }).then((imageData) => {
-    // imageData is a base64 encoded string
-      this.base64Image = "data:image/jpeg;base64," + imageData;
-  }, (err) => {
-      console.log(err);
-  });
-}
-
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
   timerFunc(){
-  
-      let alert = this.alertCtrl.create({
-        title: 'Low battery',
-        subTitle: '10% of battery remaining',
-        buttons: ['Dismiss']
-      });
-      alert.present();
-    
-    clearInterval(this.intrevalId);
-    this.hour=0;
-    this.minute=0;
-    this.second=0;
-    this.txt="started";
-    this.intrevalId = setInterval(() => {
-      this.second++;
-      if(this.second==59){
-        this.second=0;
-        this.minute++;
-      }
-      if(this.minute==59){
-        this.second=0;
-        this.minute=0;
-        this.hour++;
-      }
-      if(this.hour==23){
-        this.second=0;
-        this.minute=0;
-        this.hour=0;     
-      }
-      this.ref.detectChanges();
-    }, 1000);
+    this.temp1=true;
+    if(this.temp==true){
+      this.temp=false;
+      clearInterval(this.intrevalId);
+      this.hourR=0;
+      this.minuteR=0;
+      this.secondR=0;
+      this.hourL=0;
+      this.minuteL=0;
+      this.secondL=0;
+      this.txt="started";
+      this.intrevalId = setInterval(() => {
+        this.secondR++;
+        if(this.secondR==10){
+          this.secondR=0;
+          this.secondL++;
+        }
+        if(this.secondL==6){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR++;
+        }
+        if(this.minuteR==10){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL++;
+        }
+        if(this.minuteL==6){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL=0; 
+          this.hourR++;  
+        }
+        if(this.hourR==10){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL=0; 
+          this.hourR=0;
+          this.hourL++;  
+        }
+        if(this.hourL==2&&this.hourR==4){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL=0; 
+          this.hourR=0;
+          this.hourL=0; 
+          this.secondR++;
+        }
+        this.ref.detectChanges();
+      }, 1000);
+    }
+  }
+  reStart(){
+    if(this.temp1==true){
+      clearInterval(this.intrevalId);
+      this.hourR=0;
+      this.minuteR=0;
+      this.secondR=0;
+      this.hourL=0;
+      this.minuteL=0;
+      this.secondL=0;
+      this.txt="started";
+      this.intrevalId = setInterval(() => {
+        this.secondR++;
+        if(this.secondR==10){
+          this.secondR=0;
+          this.secondL++;
+        }
+        if(this.secondL==6){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR++;
+        }
+        if(this.minuteR==10){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL++;
+        }
+        if(this.minuteL==6){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL=0; 
+          this.hourR++;  
+        }
+        if(this.hourR==10){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL=0; 
+          this.hourR=0;
+          this.hourL++;  
+        }
+        if(this.hourL==2&&this.hourR==4){
+          this.secondR=0;
+          this.secondL=0;
+          this.minuteR=0;
+          this.minuteL=0; 
+          this.hourR=0;
+          this.hourL=0; 
+          this.secondR++;
+        }
+        this.ref.detectChanges();
+      }, 1000);
+    }
   }
   stop(){
+    this.temp=true;
+    this.temp1=false;
     clearInterval(this.intrevalId);
     this.txt="Start";
     this.intrevalId = -1;
